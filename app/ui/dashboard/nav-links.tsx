f29b6119f2ab0d3975d4606acf7aa0f97b62ac67
kaylan <guillaume.g44@hotmail.fr>
2026-02-28 13:09:52 +0800

    Add charts page

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Charts', href: '/dashboard/charts', icon: ChartsIcon },
 ];
 
 export default function NavLinks() {
